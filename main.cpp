802baab9206a2c3fc30078e55f884f56d104d52b
Fairuz Wan Ismail <wmfairuz.wanismail@gmail.com>
2012-12-07 21:55:44 +0100

    opengl: tambah fungsi reshape

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ void papar(void){
 	glFlush(); // Hantar buffer ke tetingkap
 }
  
+void reshape (int width, int height) {
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height); 
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 100.0); 
+	glMatrixMode(GL_MODELVIEW); 
+}
+ 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
@@ -20,6 +28,7 @@ int main(int argc, char** argv)
 	glutCreateWindow ("Tetingkap pertama saya");
  
 	glutDisplayFunc(papar); 
+        glutReshapeFunc(reshape);
  
 	glutMainLoop(); 
 	return 0; 
