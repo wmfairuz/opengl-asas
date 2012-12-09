6ef8c01e771b7109d017549b19488075ce840bfc
Fairuz Wan Ismail <wmfairuz.wanismail@gmail.com>
2012-12-09 11:54:44 +0100

    opengl: pergerakan dan putaran.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+bool naik = false;   // arah
+float lokasi_y = 0.0f; // lokasi pada paksi y
+float sudut_y = 0.0f;  // sudut pada paksi y
+
 void binaPrimitif(void){
 	glBegin(GL_QUADS);
 	glColor3f(0.0f, 0.0f, 1.0f); // bucu berwarna biru
@@ -25,15 +29,33 @@ void tekanKekunci(unsigned char key, int x, int y) {
 }
  
 void lepasKekunci(unsigned char key, int x, int y) {
- 
+
 }
  
 void papar(void){
+	if (naik) // arah ke atas
+		lokasi_y -= 0.0005f; // gerak ke atas pada paksi y  
+	else  // arah ke bawah  
+		lokasi_y += 0.0005f; // gerak ke bawah pada paksi y  
+	  
+	if (lokasi_y < -3.0f) // had atas  
+		naik = false; // tukar arah 
+	else if (lokasi_y > 3.0f) // had bawah 
+		naik = true; // tukar arah 
+	  
+	sudut_y += 0.005f; // putaran 
+	if (sudut_y > 360.0f) // jika lebih 360 darjah, tolak 360  
+		sudut_y -= 360.0f; 
+
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Tukar background kepada warna merah
 	glClear(GL_COLOR_BUFFER_BIT); // Kosongkan buffer warna
 	glLoadIdentity(); // 
 	glTranslatef(0.0f, 0.0f, -5.0f);  // alihkan kamera menjauhi paksi z 
  	//binaPrimitif();   // Bina bentuk primitif
+	
+	glTranslatef(0.0f, lokasi_y, 0.0f);   
+	glRotatef(sudut_y, 0.0f, 1.0f, 0.0f);
+
 	glutWireSphere(1.0f, 8.0f, 8.0f);
 	glFlush(); // Hantar buffer ke tetingkap
 }
@@ -56,12 +78,12 @@ int main(int argc, char** argv)
 	glutInitWindowPosition (100, 100);
 	glutCreateWindow ("Tetingkap pertama saya");
  
-	// Now, we define callbacks and functions for various tasks.
 	glutDisplayFunc(papar); 
+	glutIdleFunc(papar);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(tekanKekunci);
     glutKeyboardUpFunc(lepasKekunci);
  
-	glutMainLoop(); // Start the main code
-	return 0;   /* ANSI C requires main to return int. */
+	glutMainLoop();
+	return 0;   
 }
